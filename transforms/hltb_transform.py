efce07b442f8c9d86b39c3c0430a96ecd1fabc05
Kyle Anderson <kyleanderson0112@gmail.com>
2025-06-20 22:39:57 -0400

    Synchronizing job hltb_transform from AWS Glue at 2025/06/21 02:39:57

--- transforms/hltb_transform.py
+++ transforms/hltb_transform.py
@@ -1,1 +1,21 @@
 import sys
+import boto3
+import pandas as pd
+
+s3 = boto3.client('s3')
+bucket = 'rawgamedata'
+
+listOfObjects = s3.list_objects_v2(Bucket=bucket)
+
+dflist = []
+
+for obj in listOfObjects.get('Contents', []):
+    key = obj['Key']
+    if key.endswith('.csv'):
+        csv = s3.get_object(Bucket=bucket, Key=key)
+        body = csv['Body'].read().decode('utf-8')
+        df = pd.read_csv(StringIO(body))
+        dflist.append(df)
+        
+combined_df = pd.concat(dflist, ignore_index=True)
+print(combined_df.head())
